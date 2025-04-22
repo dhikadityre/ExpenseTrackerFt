890b92d28aa8361e09820e158e79b4c8d54e2c40
@algoreadme <dhika.aditya@kpvendor.id>
2025-04-22 10:45:06 +0700

    feat: create dummy data with available models

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker_ft/models/expenses.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -10,14 +11,26 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expanses> _registeredExpanses = [
+    Expanses(
+      title: 'Flutter Course',
+      amount: 19.98,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expanses(
+      title: 'Cinema',
+      amount: 15.0,
+      date: DateTime.now(),
+      category: Category.leisure,
+    )
+  ];
+
   @override
   Widget build(context) {
     return const Scaffold(
       body: Column(
-        children: [
-          Text('The Chart'),
-          Text('Expanses List')
-        ],
+        children: [Text('The Chart'), Text('Expanses List')],
       ),
     );
   }
