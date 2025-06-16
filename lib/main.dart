502e9d1557695844c0dcbf84a41b1c8f105c1487
@algoreadme <dhika.aditya@kpvendor.id>
2025-06-16 22:26:29 +0700

    feat: implement dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,38 @@ import 'package:expense_tracker_ft/widgets/expenses.dart';
 
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+
+var kDarkColorScheme =
+    ColorScheme.fromSeed(
+      seedColor: const Color.fromARGB(255, 5, 99, 125),
+      brightness: Brightness.dark, // to support dark Mode
+    );
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: kDarkColorScheme.onSecondaryContainer,
+                fontSize: 14,
+              ),
+            ),
+      ),
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
@@ -32,6 +61,7 @@ void main() {
               ),
             ),
       ), // styling theme
+      themeMode: ThemeMode.system,
       home: const Expenses(),
     ),
   );
