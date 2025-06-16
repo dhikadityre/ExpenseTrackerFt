ae89d49ad944a33f85f518d38aa2de9ae473c68b
@algoreadme <dhika.aditya@kpvendor.id>
2025-06-16 14:10:52 +0700

    feat: dismiss bottomsheet after saving data and fixing overlap keyboard with scrollview on BottomSheet

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -30,6 +30,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _didOpenExpenseOverlay() {
     showModalBottomSheet(
+      isScrollControlled: true,
       context: context, 
       builder: (ctx) => NewExpense(onAddExpense: _didAddExpense),
     );
