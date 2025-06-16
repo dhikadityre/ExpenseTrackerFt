2365a43c6c7f6496b2e4639fbfe4de03bd8bb30e
@algoreadme <dhika.aditya@kpvendor.id>
2025-06-16 11:52:25 +0700

    showing Date Picker and layouting positional DatePicker with mainAxis dan crossAxis

--- lib/widgets/bottomsheets/new_expense.dart
+++ lib/widgets/bottomsheets/new_expense.dart
@@ -13,6 +13,19 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _priceController = TextEditingController();
 
+  void _presentDatePicker() {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+
+
+    showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+  }
+
   @override
   void dispose() {
     _titleController.dispose();
@@ -34,18 +47,40 @@ class _NewExpenseState extends State<NewExpense> {
               label: Text('Title'),
             ),
           ),
-          TextField(
-            controller: _priceController,
-            maxLength: 12,
-            keyboardType: TextInputType.number,
-            decoration: const InputDecoration(
-              prefixText: 'Rp ',
-              label: Text('Amount'),
-            ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _priceController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    prefixText: 'Rp ',
+                    label: Text('Amount'),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end, // horizontal-end
+                  crossAxisAlignment:
+                      CrossAxisAlignment.center, // vertical-center
+                  children: [
+                    const Text('Selected Date'),
+                    IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: const Icon(Icons.calendar_month),
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
           Row(
             children: [
-              Spacer(),
+              const Spacer(),
               TextButton(
                 onPressed: () {
                   Navigator.pop(context);
